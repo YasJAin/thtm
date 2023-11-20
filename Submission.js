55eae8b7ccc94c69a9238e24c64ee6dc9f3cf119
Yash Jain <129750045+YasJAin@users.noreply.github.com>
2023-11-20 13:45:58 +0000

    Add files via upload

---
+++ Submission.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import { InputGroup, Row } from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+
+function Submission() {
+    const allEntries = JSON.parse(localStorage.getItem("allEntries"));
+    const [displayDetail, setDisplay] = useState(false);
+    const [singleEntry, setSingleEntry] = useState({'name': '', 'email': '','phone': '', 'checkbox_values':[]})
+    
+
+    useEffect(() => {
+        var id,entry;
+        if (!window.location.pathname.includes('submissions')){
+            setDisplay(true)
+            id = window.location.pathname.split('/').pop()
+            entry = allEntries.filter(item => parseInt(item['id']) === parseInt(id))[0]
+            setSingleEntry(entry)
+        }
+    },[]);
+
+    const handleCheckVal = (ty,entry) =>{
+        var val =''
+        if (entry['checkbox_values'].length >0 ){
+            val = entry['checkbox_values'].filter(item => item.split('_')[0] === ty)[0]
+            val = val.split('_')[1]
+        }
+        return val
+    }
+    const singleEntryForm = ()=>{
+        return(
+            <h3>
+            <Container>
+                <Card>
+                    <Card.Header>
+                        <cite title="Source Title">Feedback Details
+                        </cite>
+                    </Card.Header>
+                    <Card.Body>
+                        <Row>
+                            <Col>Customer Name</Col>
+                            <Col>{singleEntry['name']}</Col>
+                        </Row>
+                        <Row>
+                            <Col>Email</Col>
+                            <Col>{singleEntry['email']}</Col>
+                        </Row>
+                        <Row>
+                            <Col>Phone</Col>
+                            <Col>{singleEntry['phone']}</Col>
+                        </Row>
+                        {Object.keys(feedback_type).map((ty)=>(
+                            <Row>
+                                <Col>{feedback_type[ty]}</Col>
+                                <Col>{handleCheckVal(ty,singleEntry)}</Col>
+                            </Row>
+                        ))}
+                    </Card.Body>
+                </Card>
+            </Container>
+            </h3>
+        )
+    }
+    
+    const feedback_type = {
+        'qos': 'Please rate the quality of the service you received.', 
+        'qob': 'Please rate the quality of your car services.',
+        'roc': 'Was our car clean?',
+        'exp': 'Please rate your overall riding experience.'
+    }
+    
+    return (
+        <>
+        {displayDetail?
+            (singleEntryForm())
+            :
+            (<div className='padding30px'>
+                <Table striped hover responsive>
+                    <thead>
+                        <tr>
+                            <th>Form Details</th>
+                            <th>Customer Name</th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                            {Object.keys(feedback_type).map((ty)=>(<th>{feedback_type[ty]}</th>))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {allEntries.map(entry=>(
+                            <tr>
+                                <td><a href={`/submission/${entry['id']}`} target="_blank" rel="noopener noreferrer">View Details</a></td>
+                                <td>{entry['name']}</td>
+                                <td>{entry['email']}</td>
+                                <td>{entry['phone']}</td>
+
+                                {Object.keys(feedback_type).map((ty)=>(
+                                    <td>{handleCheckVal(ty,entry)}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </div>)
+        }
+        </>
+    );
+}
+
+export default Submission;
